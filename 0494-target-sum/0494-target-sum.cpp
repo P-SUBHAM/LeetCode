16edb42ce6be99c45eb2fade3bf5c41ca77cdba2
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2024-12-27 13:45:06 +0530

    Time: 169 ms (44.25%), Space: 65.3 MB (8.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
-    // APPROACH-2
+    // APPROACH-2.2
     // DP using memoization
-    map<pair<int,int>,int> dp;
+    // using unordered map with hash for pair type
+    struct pair_hash {
+        template <class T1, class T2>
+        size_t operator()(const pair<T1, T2>& p) const {
+            auto h1 = hash<T1>{}(p.first);
+            auto h2 = hash<T2>{}(p.second);
+            return h1 ^ (h2 << 1); // Combine the hashes
+        }
+    };
+    
+    unordered_map<pair<int,int>,int,pair_hash> dp;
     int findTargetSumWaysf(vector<int> &nums, int target, int ind) {
         int n = nums.size();
         if(ind == n - 1) {
@@ -24,3 +34,17 @@ public:
         return findTargetSumWaysf(nums,target,0);
     }
 };
+
+// FOR vector and set
+// template <typename Container>
+// struct container_hash {
+//     template <typename T>
+//     size_t operator()(const Container& c) const {
+//         size_t hash_val = 0;
+//         for (const auto& elem : c) {
+//             hash_val ^= hash<T>{}(elem) + 0x9e3779b9 + (hash_val << 6) + (hash_val >> 2);
+//         }
+//         return hash_val;
+//     }
+// };
+// unordered_map<pair<int,int>,int,container_hash<vector<int>>> dp;
