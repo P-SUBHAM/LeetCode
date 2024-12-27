c4ffd88c517c5d7e0b5f723dc89a2c168040165e
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2024-12-27 13:36:24 +0530

    Time: 262 ms (38.99%), Space: 62.6 MB (9.3%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    // APPROACH-2
+    // DP using memoization
+    map<pair<int,int>,int> dp;
     int findTargetSumWaysf(vector<int> &nums, int target, int ind) {
         int n = nums.size();
         if(ind == n - 1) {
             if(target == nums[ind] or target == -1*nums[ind]) {
                 if(nums[ind] == 0) {
-                    return 2;
+                    return dp[{target,ind}] = 2;
                 }
-                return 1;
+                return dp[{target,ind}] = 1;
             }
-            return 0;
+            return dp[{target,ind}] = 0;
         }
-        return findTargetSumWaysf(nums,target-nums[ind],ind+1) + findTargetSumWaysf(nums,target+nums[ind],ind+1);
+        if(dp.find({target,ind})!=dp.end()) {
+            return dp[{target,ind}];
+        }
+        return dp[{target,ind}] = findTargetSumWaysf(nums,target-nums[ind],ind+1) + findTargetSumWaysf(nums,target+nums[ind],ind+1);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        dp.clear();
         return findTargetSumWaysf(nums,target,0);
     }
 };
