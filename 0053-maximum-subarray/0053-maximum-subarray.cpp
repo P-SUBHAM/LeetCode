3c826cea5c48a89fbc6e9c046eeb97534e59a100
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-07 00:30:27 +0530

    Time: 0 ms (100%), Space: 71.7 MB (57.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,13 @@
+// Approach Kadane Algo
+// sum = best(sum+a[i],a[i])
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // ans is global curr is local
-        // cur is sum from last subarray, if -ve wont contribute in future so drop
-        // initially if cursum - -ve adjust
-        int ans = nums[0], currsum = max(0,nums[0]);
-        int n = nums.size();
+        int best = nums[0], sum = nums[0], n = nums.size();
         for(int i = 1; i < n; i++) {
-            currsum += nums[i];
-            ans = max(ans,currsum);
-            if(currsum < 0) {
-                currsum = 0;
-            }
+            sum = max(sum+nums[i],nums[i]);
+            best = max(best,sum);
         }
-        return ans;
+        return best;
     }
 };
