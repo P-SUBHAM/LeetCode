e57bf58328a4783399097a057a84829da8096898
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-06 13:30:29 +0530

    Time: 2364 ms (5%), Space: 72 MB (8.3%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -2,6 +2,10 @@
 class Solution {
 public:
   int findKthLargest(vector<int>& nums, int k) {
+    return QuickSelect(nums,k);
+  }
+  // Aproach 1: QuickSelect O(n -> n^2)
+  int QuickSelect(vector<int>& nums, int k) {
     auto partition = [&](int l, int h) -> int {
       int pivot = nums[h], i = l-1;
       for(int j = l; j < h; j++) {
@@ -13,16 +17,16 @@ public:
       swap(nums[i+1],nums[h]);
       return i+1;
     }; 
-    function<int(int, int, int)> QuickSelect = [&](int l, int h,int k) -> int {
+    function<int(int, int, int)> QSelect = [&](int l, int h,int k) -> int {
       int pi = partition(l, h);
       if (pi == k) {
         return nums[k];
       } else if (pi < k) {
-        return QuickSelect(pi+1, h, k);
+        return QSelect(pi+1, h, k);
       } else {
-        return QuickSelect(l, pi - 1, k);
+        return QSelect(l, pi - 1, k);
       }
     };
-    return QuickSelect(0,nums.size()-1,nums.size()-k);
+    return QSelect(0,nums.size()-1,nums.size()-k);
   }
 };
