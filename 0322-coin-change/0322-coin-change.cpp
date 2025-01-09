8f3a74c151804d6718d5133d7deadff65e6053b3
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-09 18:18:30 +0530

    Time: 19 ms (81.66%), Space: 17.8 MB (70.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,55 @@
-// Approach 1 memoization
+// Apprpach 2: Tabular 2D
 class Solution {
 public:
-    map<int,int> dp;
-    int dpf(vector<int>& coins, int amount) {
-        int n = coins.size(), ans = 1e9;
-        if(amount < 0) {return 1e9;}
-        if(amount == 0) {
-            return 0;
-        }
-        if(dp.find(amount) != dp.end()) {
-            return dp[amount];
-        }
-        for(int i = 0; i < n; i++) {
-            if(amount-coins[i] >= 0) {
-                ans = min(ans,1+dpf(coins,amount-coins[i]));
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+        int n = coins.size();
+        vector<int> dp(amount+1,1e9);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < coins.size(); j++) {
+                if(i - coins[j] >= 0) {
+                    dp[i] = min(dp[i],1+dp[i-coins[j]]);
+                }
+                else {
+                    break;
+                }
             }
         }
-        return dp[amount] = ans;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        int ans = dpf(coins,amount);
-        if(ans == 1e9) {
-            return -1;
-        }
-        else {
-            return ans;
-        }
+        if(dp[amount] == 1e9) {return -1;}
+        return dp[amount];
     }
 };
+
+// vector<int> dp;
+// // Approach 1 memoization
+// class Solution {
+// public:
+//     int dpf(vector<int>& coins, int amount) {
+//         int n = coins.size(), ans = 1e9;
+//         if(amount < 0) {return 1e9;}
+//         if(amount == 0) {
+//             return 0;
+//         }
+//         if(dp[amount] != 1e9) {
+//             return dp[amount];
+//         }
+//         for(int i = 0; i < n; i++) {
+//             if(amount-coins[i] >= 0) {
+//                 ans = min(ans,1+dpf(coins,amount-coins[i]));
+//             }
+//         }
+//         return dp[amount] = ans;
+//     }
+//     int coinChange(vector<int>& coins, int amount) {
+//         dp.clear();
+//         dp.resize(amount+1,1e9);
+//         int ans = dpf(coins,amount);
+//         if(ans == 1e9) {
+//             return -1;
+//         }
+//         else {
+//             return ans;
+//         }
+//     }
+// };
