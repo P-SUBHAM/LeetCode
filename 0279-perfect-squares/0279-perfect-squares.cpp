f22b007947dfbe22715eb56dc1672e53db592f39
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-09 14:42:32 +0530

    Time: 8 ms (96.97%), Space: 10.5 MB (88.29%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,10 +1,11 @@
-// Approach 1: memoization
+// Approach 1.1 : memoization with vector
 class Solution {
 public:
-    unordered_map<int,int> dp;
+    vector<int> dp;
     int numSquares(int n) {
+        static vector<int> dp(1e4 + 1, -1);
         if(n==0) return 0;
-        if(dp.find(n) != dp.end()) {
+        if(dp[n] != -1) {
             return dp[n];
         }
         int ans = 1e9;
@@ -14,3 +15,20 @@ public:
         return dp[n] = ans;
     }
 };
+
+// // Approach 1: memoization with map
+// class Solution {
+// public:
+//     unordered_map<int,int> dp;
+//     int numSquares(int n) {
+//         if(n==0) return 0;
+//         if(dp.find(n) != dp.end()) {
+//             return dp[n];
+//         }
+//         int ans = 1e9;
+//         for(int i = 1; i*i <= n; i++) {
+//             ans = min(ans,1 + numSquares(n-i*i));
+//         }
+//         return dp[n] = ans;
+//     }
+// };
