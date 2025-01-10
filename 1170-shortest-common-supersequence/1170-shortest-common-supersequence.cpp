30dc6ff61bb043e0cf0b6dbd13471dac6298e166
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-10 16:34:52 +0530

    Time: 26 ms (56.12%), Space: 28.9 MB (11.83%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,77 @@
+// Approach 1: using string lcs
+// find lcs, str1,str2,lcs keep adding char from str1,2 as long as not matching lcs, once matched add once, then repeat till u have added all the chars from both str1,2
+// Approach correct but memeory limit exceeded
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        string lcs = longestCommonSubsequence(str1,str2), ans = "";
+        int i = 0, j = 0, k = 0, n1 = str1.size(), n2 = str2.size(), n3 = lcs.size();
+        while(i < n1 or j < n2) {
+            if(k != n3) {
+                if(str1[i] != lcs[k]) {
+                    ans += str1[i]; i++; continue;
+                }
+                if(str2[j] != lcs[k]) {
+                    ans += str2[j]; j++; continue;
+                }
+                ans += lcs[k];
+                k++; i++; j++;
+            }
+            else {
+                if(i < n1) {
+                    ans += str1[i]; i++;
+                }
+                if(j < n2) {
+                    ans += str2[j]; j++;
+                }
+            }
+        }
+        return ans;
+    }
+    string longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        for(int i = 1; i <= n1; i++) { // DP table tabulation
+            for(int j = 1; j <= n2; j++) {
+                if(text1[i-1] == text2[j-1]) {dp[i][j] = 1 + dp[i-1][j-1];}
+                else {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        string ans = "";
+        int i = n1, j = n2;
+        while(i > 0 && j > 0) {
+            if(text1[i-1] == text2[j-1]) {
+                ans = (text1[i-1] + ans); i--; j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1]) {
+                i--;
+            }
+            else {
+                j--;
+            }
+        }
+        cout<<ans;
+        return ans;
+    }
+
+};
+/*
+Storing vector<vector<string>> for each index giving memory error
+string longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<string>> dp(n1, vector<string>(n2, "-1"));
+        function<string(int, int)> dpf = [&](int i, int j) -> string {
+            if(i == -1 or j == -1) {return "";}
+            if(dp[i][j] != "-1") {return dp[i][j];}
+            if(text1[i] == text2[j]) {return dp[i][j]=(dpf(i-1,j-1) + text1[i]);}
+            string s1 = dpf(i-1,j), s2 = dpf(i,j-1);
+            if(s1.size() > s2.size()) {return dp[i][j]=s1;}
+            else {return dp[i][j]=s2;}
+        };
+        string ans = dpf(n1 - 1, n2 - 1);
+        // cout<<ans;
+        return ans;
+    }
+*/
