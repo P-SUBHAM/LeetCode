8edc334ada21bf41179e75804decadc0961cd719
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-10 13:02:33 +0530

    Time: 97 ms (5.13%), Space: 29 MB (5.58%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,7 +3,29 @@
 [:i][:j] = (i==j) + [:i-1][:j-1] or max([:i][:j-1],[:i-1]][:j])
 if i,j th char match count for i-1,j-1 else exclude once i or j 
 */
+
 class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        function<int(int, int)> dpf = [&](int i, int j) -> int {
+            if (i == -1 || j == -1) {
+                return 0;
+            }
+            if (dp[i][j] != -1) {
+                return dp[i][j];
+            }
+            if (text1[i] == text2[j]) {
+                return dp[i][j] = 1 + dpf(i - 1, j - 1);
+            }
+            return dp[i][j] = max(dpf(i - 1, j), dpf(i, j - 1));
+        };
+        return dpf(n1 - 1, n2 - 1);
+    }
+};
+
+class Solution1 {
 public:
     vector<vector<int>> dp;
     int dpf(string &text1, string &text2, int i, int j) {
