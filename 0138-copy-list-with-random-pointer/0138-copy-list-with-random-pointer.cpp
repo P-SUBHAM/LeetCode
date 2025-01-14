78771d8496c6c43019f4efa5a94bd08629742625
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-15 04:01:52 +0530

    Time: 3 ms (91.53%), Space: 15.1 MB (43.6%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+// Approach 1: Iterative
+// Create Node clone(val) make p->next = newp
+// 1 iter to fix random ptr links
+// 1 iter to fix next links
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node *p = head;
+        if(head == NULL) return head;
+        while(p != NULL) {
+            Node *newp = new Node(p->val); // creating deepcopy nodes
+            newp->next = p->next;
+            p->next = newp;
+            p = p->next->next;
+        }
+        p = head;
+        Node *newhead = p->next;
+        while(p != NULL) { // move random ptr links
+            if(p->random != NULL) p->next->random = p->random->next;
+            p = p->next->next;
+        }
+        p = head;
+        while(p->next->next != NULL) { // moves next ptr links
+            Node *newp = p->next;
+            p->next = newp->next; // og next link restore
+            newp->next = p->next->next;
+            p = p->next;
+        }
+        p->next = NULL;
+        return newhead;
+    }
+};
