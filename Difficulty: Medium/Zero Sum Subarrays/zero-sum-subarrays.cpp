cea68dc19aff07e9a3b0de1831b9e777adb15b76
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-15 22:53:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findSubarray(vector<int> &arr) {
+        unordered_map<int,int> mp;
+        int x = 0, ans = 0; mp[0] = 1;
+        for(auto it: arr) {
+            x += it;
+            ans += mp[x];
+            mp[x]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        cout << ob.findSubarray(arr) << endl;
+        cout << "~\n";
+    }
+}
+
+// } Driver Code Ends
