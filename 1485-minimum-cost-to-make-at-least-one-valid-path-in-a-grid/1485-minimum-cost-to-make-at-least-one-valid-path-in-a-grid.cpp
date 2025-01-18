b04f378315ebaf90a0fedbe159074538933e50fa
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-18 10:41:38 +0530

    Time: 22 ms (64.7%), Space: 18.1 MB (92.12%) - LeetHub

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,116 @@
+// Approach 2: Using Graph
+// Shortest-path problem: each cell is node, and moving in dir: 0 cost, change dir: 1 cost. 
+// Using a priority queue (BFS with Dijkstra-like algorithm) ensures the lowest-cost path is always explored first
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> cost(m, vector<int>(n, INT_MAX)); // Min cost to reach each cell
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+
+        vector<int> dx = {0, 0, 1, -1}; // Directions: right, left, down, up
+        vector<int> dy = {1, -1, 0, 0};
+
+        pq.push({0, {0, 0}}); // {cost, {row, col}}
+        cost[0][0] = 0;
+
+        while (!pq.empty()) {
+            auto [curCost, cell] = pq.top();
+            pq.pop();
+            int x = cell.first, y = cell.second;
+
+            if (x == m - 1 && y == n - 1) {
+                return curCost; // Reached destination
+            }
+
+            if (curCost > cost[x][y]) {
+                continue; // Skip if we've already found a cheaper path
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d], ny = y + dy[d];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                    int newCost = curCost + (grid[x][y] != d + 1); // +1 if changing direction
+                    if (newCost < cost[nx][ny]) {
+                        cost[nx][ny] = newCost;
+                        pq.push({newCost, {nx, ny}});
+                    }
+                }
+            }
+        }
+
+        return -1; // Shouldn't reach here
+    }
+};
+
+class Solution1 {
+public:
+    // Approach 1: Used DP
+    int m,n; int global_ans; // vector<vector<int>> grid;
+    map<pair<pair<int,int>,vector<vector<bool>>>,int> dp;
+    vector<int> neiF(int i, int j, vector<vector<bool>> vis) {
+        vector<int> neipos;
+        if(0 <= i+1 && i+1 <= m-1 && !vis[i+1][j]) {
+            neipos.push_back(3);
+        }
+        if(0 <= j+1 && j+1 <= n-1 && !vis[i][j+1]) {
+            neipos.push_back(1);
+        }
+        if(0 <= i-1 && i-1 <= m-1 && !vis[i-1][j]) {
+            neipos.push_back(4);
+        }
+        if(0 <= j-1 && j-1 <= n-1 && !vis[i][j-1]) {
+            neipos.push_back(2);
+        }
+        return neipos;
+    }
+    vector<int> coorF(int i, int j, int dir) {
+        if(dir == 1) {
+            return {i,j+1};
+        }
+        if(dir == 2) {
+            return {i,j-1};
+        }
+        if(dir == 3) {
+            return {i+1,j};
+        }
+        if(dir == 4) {
+            return {i-1,j};
+        }
+        return {};
+    }
+    int dpF(vector<vector<int>> grid, int i, int j, vector<vector<bool>> vis) {
+        if(i == m-1 && j == n-1) {
+            return 0;
+        }
+        if(i < 0 or i >= m or j < 0 or j >= n) {
+            return 1e5;
+        }
+        if(dp.find({{i,j},vis}) != dp.end()) {
+            return dp[{{i,j},vis}];
+        }
+        int x = grid[i][j];
+        vis[i][j] = true;
+        vector<int> nei = neiF(i,j,vis);
+        int ans = 1e5;
+        for(auto it: nei) {
+            if(it == x) {
+                vector<int> coor = coorF(i,j,it);
+                ans = min(ans,dpF(grid,coor[0],coor[1],vis));
+            }
+            else {
+                vector<int> coor = coorF(i,j,it);
+                ans = min(ans,1 + dpF(grid,coor[0],coor[1],vis));
+            }
+        }
+        return dp[{{i,j},vis}] = ans;
+    }
+    int minCost(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        dp.clear(); //dp.resize(m+1,vector<int> (n+1,-1));
+        vector<vector<bool>> vis(m,vector<bool> (n,false));
+        int ans = dpF(grid,0,0,vis);
+        // cout<<"ans:"<<ans<<endl;
+        return ans;
+    }
+};
