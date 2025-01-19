fbb6036bc7ea42b2ff984d12eb84b6de3b85634e
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-19 17:16:43 +0530

    Update minimum-spanning-tree.cpp
    added comments for prims algo GFG

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -18,29 +18,22 @@ class Solution {
         int ans = 0;
         vector<bool> vis(V,false);
         // vis array required to make sure one node incl in MStree during pop is not inluded again
-        
-        // we relax only during pop as we are sure then node,wt edge will be included in MST
+        // we relax only during pop as we are sure, then edge(wt,node) will be included in MST 
         // no need for relax during pushing into PQ
-        
         // nei node makes the overall scene better, push only if relaxing
-        // *Note: Maintain vis, update after pop, dont update before push just check;
-        while(!pq.empty()) { // in pq : u update only when pop, not when push
+        // *Note: Maintain vis, update after pop, dont update before pushing into PQ just check;
+        while(!pq.empty()) { // in pq : relax only when pop, not when push
             auto it = pq.top(); pq.pop();
             int a = it[1], wa = it[0];
-            
             if(vis[a]) continue;
             else vis[a] = true;
-            
-            dist[a] = wa;
-            ans += wa;
-            // for(auto itd: dist) {
-            //     cout<<itd<<" ";
-            // }cout<<endl;
+            dist[a] = wa; // update both dist(main) and ans
+            ans += wa; // direct ans
             for(auto nei: adj[a]) {
                 int b = nei[0], wb = nei[1];
-                // optimise during push, but update at pop only
+                // check optimise cond during push, but update at pop only
                 if(wb < dist[b]) {
-                    pq.push({wb,b});
+                    pq.push({wb,b}); // pushing wb not dist[b] as wb smaller, and dist[b] not updated
                 } 
             }
         }
