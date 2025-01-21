6ff8dcaedb86ccded3b3ae6a66afac0702818a9c
P.Subham <p.subham.software@gmail.com>
2025-01-21 12:36:17 +0530

    added TLE for dsu

--- 0000-DSA/Graph/DSU.cpp
+++ 0000-DSA/Graph/DSU.cpp
@@ -1,7 +1,20 @@
+// Simplest DSU implementation O(nlogn)
+struct dsu {
+    unordered_map<int,int> p;
+    int find(int x) {
+        if(p.find(x)==p.end()) return p[x] = x;
+        return (p[x] == x) ? x : p[x] = find(p[x]);
+    }
+    void join(int x, int y) {
+        p[find(x)]=find(y);
+    }
+};
+// annecdote: map: TLE unordered_map:906ms 
+
 // DSU with path Compression and Union by rank
 struct dsu {
-    map<int, int> p; // Parent map
-    map<int, int> rank; // Rank map (optional, for union by rank)
+    unordered_map<int, int> p; // Parent map
+    unordered_map<int, int> rank; // Rank map (optional, for union by rank)
     int find(int x) {
         if (p.find(x) == p.end()) return p[x] = x; // Lazy initialization
         return (p[x] == x) ? x : p[x] = find(p[x]); // Path compression
@@ -15,3 +28,4 @@ struct dsu {
         }
     }
 };
+// annecdote: map: 1999ms unordered_map: 890ms
