0ab1deb8f73875d0aeaf9c4c7f3b1c759b6baaab
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-22 09:49:57 +0530

    Time: 277 ms (5.05%), Space: 102.3 MB (5.15%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,52 @@
+// Approach 1: MultiSource BFS
+class Solution1 {
+public:
+    vector<vector<int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}};
+    vector<vector<int>> highestPeak(vector<vector<int>>& grid) {
+        queue<vector<int>> qu;
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    qu.push({i,j,0});
+                    vis[i][j] = true;
+                }
+            }
+        }
+        function<bool(int,int)> valid = [&](int r, int c) {
+            return 0 <= r && r < m && 0 <= c && c < n && !vis[r][c];
+        };
+        vector<vector<int>> res(m,vector<int>(n,0));
+        while(!qu.empty()) {
+            vector<int> v = qu.front(); qu.pop();
+            int v_x = v[0];
+            int v_y = v[1];
+            int v_d = v[2];
+            res[v_x][v_y] = v_d; // set dist from water, update here only
+            for(auto dir: dirs) {
+                int n_x = v_x + dir[0];
+                int n_y = v_y + dir[1];
+                int n_d = v_d + 1;
+                // cout<<v_x<<":"<<v_y<<" ";
+                if(valid(n_x,n_y)) {
+                    qu.push({n_x,n_y,n_d});
+                    vis[n_x][n_y] = true;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// Same as 1765, only need to invert 0-1
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        Solution1 sol;
+        for(auto &r: mat) 
+            for(auto &it: r)
+                it ^= 1;
+        return sol.highestPeak(mat);
+    }
+};
