cc87cb105a108fa5b82c0f7783152728650518e6
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-24 10:54:40 +0530

    Time: 1133 ms (5.03%), Space: 393.8 MB (5.02%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -12,12 +12,57 @@ public:
             if(!vis[nei]) {
                 if(is_cycle(nei,vis,stack,cycle)) {
                     vis[src] = false;
-                    stack[src] = false;
+                    // stack[src] = false;
                     return cycle[nei] = true;
                 }
             }
             if(stack[nei]) {
                 vis[src] = false;
+                // stack[src] = false;
+                return cycle[nei] = true;
+            }
+        }
+        stack[src] = false;
+        return cycle[src] = false; // return no cycle
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        adj = graph;
+        vector<bool> vis(n,false);
+        vector<bool> stack(n,false);
+        vector<bool> cycle(n,false);
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                cycle[i] = is_cycle(i,vis,stack,cycle);
+            }
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            if(!cycle[i]) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution1 {
+public:
+    vector<vector<int>> adj;
+    bool is_cycle(int src, vector<bool> &vis,vector<bool> &stack,vector<bool> cycle) {
+        if(vis[src]) return cycle[src];
+        vis[src] = true;
+        stack[src] = true; // maintain recursion stack
+        for(auto nei: adj[src]) {
+            if(!vis[nei]) {
+                if(is_cycle(nei,vis,stack,cycle)) {
+                    vis[src] = false; // mark both vis and stack as false
+                    stack[src] = false;
+                    return cycle[nei] = true;
+                }
+            }
+            if(stack[nei]) {
+                vis[src] = false; // mark both vis and stack as false
                 stack[src] = false;
                 return cycle[nei] = true;
             }
