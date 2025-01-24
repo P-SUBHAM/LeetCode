03aa1a13aeafb02f0eedd269185154569c29176d
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-24 14:37:31 +0530

    Time: 3 ms (28.93%), Space: 11.7 MB (23.87%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,11 +8,17 @@ public:
         }
         // swap l th element with all elements on right[l to r] and call the recursion
         for(int j = l; j <= r; j++) {
-            vector<int> newnums;
-            newnums = nums;
-            swap(newnums[l],newnums[j]);
-            AllPermuteRec(newnums,l+1,r);
+            swap(nums[l],nums[j]);
+            AllPermuteRec(nums,l+1,r);
+            swap(nums[j],nums[l]);
         }
+        // for(int j = l; j <= r; j++) {
+        //     vector<int> newnums;
+        //     newnums = nums;
+        //     swap(newnums[l],newnums[j]);
+        //     AllPermuteRec(newnums,l+1,r);
+        // }
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
