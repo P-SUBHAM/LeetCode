cac3a82f6db41247f18c24a54fa6c0f700d4ae36
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-01-24 11:00:34 +0530

    Time: 19 ms (65.73%), Space: 59 MB (62.48%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,7 +1,50 @@
 // Approach 1: Graph Cycle Detection in Directed Graph
 // Vis all unvisit nodes and check cycle present or not
 // maintain a current recursion stack vis and check
+
 class Solution {
+public:
+    vector<vector<int>> adj;
+
+    bool is_cycle(int src, vector<bool> &vis,vector<bool> &stack,vector<bool> &cycle) {
+        if(vis[src]) return cycle[src];
+        vis[src] = true;
+        stack[src] = true; // maintain recursion stack
+        for(auto nei: adj[src]) {
+            if(!vis[nei]) {
+                if(is_cycle(nei,vis,stack,cycle)) {
+                    return cycle[nei] = true;
+                }
+            }
+            if(stack[nei]) {
+                return cycle[nei] = true;
+            }
+        }
+        stack[src] = false;
+        return cycle[src] = false; // return no cycle
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        adj = graph;
+        vector<bool> vis(n,false);
+        vector<bool> stack(n,false);
+        vector<bool> cycle(n,false);
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                cycle[i] = is_cycle(i,vis,stack,cycle);
+            }
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            if(!cycle[i]) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution2 {
 public:
     vector<vector<int>> adj;
     bool is_cycle(int src, vector<bool> &vis,vector<bool> &stack,vector<bool> cycle) {
