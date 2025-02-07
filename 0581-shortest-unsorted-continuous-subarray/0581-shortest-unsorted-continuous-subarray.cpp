5dc1dc28d8200e76cab16594d607323d5fbf3ad1
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-02-07 13:31:29 +0530

    Time: 0 ms (100%), Space: 30.4 MB (79.52%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,6 +1,28 @@
+// Approach 2: 2ptr O(n)
+// select sorted left and right part
+// now iterate in middle and check if anything can expand l to left or r to right i.e if we get smaller than arr[l] or larger than arr[r]
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size(), l = 0, r = n-1;
+        while(l+1 < n && nums[l]<=nums[l+1]) l++;
+        while(r-1 >= 0 && nums[r]>=nums[r-1]) r--;
+        if(l >= r) return 0;// already sorted
+        int L = l, R = r;
+        for(int i = L; i <= R; i++) {
+            if(l > 0 && nums[i] < nums[l-1]) {
+                l--;i--;
+            }
+            if(r < n-1 && nums[i] > nums[r+1]) {
+                r++;i--;
+            }
+        }
+        return r-l+1;
+    }
+};
 // Approach 1: sorting+2ptr
 // sort the array and compare from left and right how many ele didn't needed sorting
-class Solution {
+class Solution1 {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         vector<int> arr = nums;
