d46c3b5c34d96d94764f1bf66450addad2a942df
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-02-10 19:16:08 +0530

    Time: 97 ms (14.53%), Space: 76.1 MB (21.13%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -5,13 +5,13 @@ start from both end, f(i,j) = longest common subsequence between w1[:i] &w2[:j]
 [:i][:j] = (i==j) + [:i-1][:j-1] or max([:i][:j-1],[:i-1]][:j])
 if i,j th char match count for i-1,j-1 else exclude once i or j 
 */
-class Solution {
+class Solution1 {
 public:
     int longestPalindromeSubseq(string s) {
         string rev = s; reverse(rev.begin(),rev.end());
         return longestCommonSubsequence(s,rev);
     }
-
+    // If match include else max(skip i or j)
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size(), n2 = text2.size();
         vector<vector<int>> dp(n1, vector<int>(n2, -1));
@@ -24,5 +24,27 @@ public:
             return dp[i][j] = max(dpf(i - 1, j), dpf(i, j - 1));
         };
         return dpf(n1 - 1, n2 - 1);
+    }
+};
+
+//Approach 2: Iterative DP
+// LCS(s,rev(s)) = longest pallindromic subsequence
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        string s1 = s, s2 = s; reverse(s2.begin(),s2.end());
+        for(int i = 1; i <= n; i++) { // i char from start
+            for(int j = 1; j <= n; j++) { // j from end
+                if(s1[i-1]==s2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
     }
 };
