9b0c74746e1e40b37e68151849038801251b9ec5
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-04 16:49:00 +0530

    Time: 3 ms (13.61%), Space: 7.9 MB (19.48%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,6 +1,23 @@
+// Approach 2: BitManip
+// represent n in ternary expression, check if it can be expressed as power of 3
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        vector<int> expression;
+        while(n > 0) {
+            expression.push_back(n%3); 
+            n /= 3;
+        }
+        reverse(expression.begin(),expression.end());
+        for(auto it: expression) cout<<it<<" "; cout<<endl;
+        for(auto it: expression) if(it == 2) return false;
+        return true;
+    }
+};
+
 // Approach 1: Recursion
 // 3^x grows exponentially either include a power or skip it
-class Solution {
+class Solution1 {
 public:
     bool power3(int n, int x) {
         if(pow(3,x) > n) return false;
