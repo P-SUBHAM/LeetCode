c102073d86fea54f9dd9830bb7d8f9fbabb81cb5
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-19 02:34:57 +0530

    Time: 43 ms (47.79%), Space: 75.8 MB (8.48%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// 1h1 2 3s N
+// 1 2s 3f N
+// 1 2 N
+// 4h2 5 N
+class Solution {
+public:
+  ListNode* sortList(ListNode* head) {
+    if (head == NULL || head->next == NULL)
+      return head;
+    ListNode *slow = head, *fast = head;
+    while (fast != NULL && fast->next != NULL && fast->next->next != NULL) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    ListNode *head1 = head, *head2 = slow->next; // spliting to halves
+    slow->next = NULL;                           // breaking the link
+    head1 = sortList(head1);
+    head2 = sortList(head2);
+    ListNode* dummy = new ListNode;
+    ListNode* ptr = dummy;
+    while (head1 != NULL && head2 != NULL) {
+      if(head1->val < head2->val) {
+        ptr->next = head1;
+        ptr = ptr->next;
+        head1 = head1->next;
+      }
+      else {
+        ptr->next = head2;
+        ptr = ptr->next;
+        head2 = head2->next;
+      }
+    }
+    while(head1 != NULL) {
+      ptr->next = head1;
+      ptr = ptr->next;
+      head1 = head1->next;
+    }
+    while(head2 != NULL) {
+      ptr->next = head2;
+      ptr = ptr->next;
+      head2 = head2->next;
+    }
+    return dummy->next;
+  }
+};
