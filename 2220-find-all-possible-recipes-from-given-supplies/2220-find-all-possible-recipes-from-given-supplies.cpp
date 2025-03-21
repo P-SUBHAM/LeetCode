3610c31a52f956bd6ad60f006d44e401fc9db4c1
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-21 12:35:49 +0530

    Time: 85 ms (73.29%), Space: 102 MB (87.34%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,5 +1,59 @@
-// Approach 1: Topo Sort
 class Solution {
+public:
+    void can_cook_supplies(unordered_map<string,bool> &can_cook, vector<string>& supplies) {
+        for(auto it: supplies) {
+            can_cook[it] = true;
+        }
+    }
+    void enumerateRecipe(vector<string>& recipes, unordered_map<string,int> &recipe_idx) {
+        int n = recipes.size();
+        for(int i = 0; i < n; i++) {
+            recipe_idx[recipes[i]] = i;  
+        }
+    }
+
+    bool dfs(vector<string>& recipes, vector<vector<string>>& ingredients, unordered_map<string,int> &recipe_idx, string item, unordered_map<string,bool> &can_cook) {
+        if(recipe_idx.find(item) == recipe_idx.end()) { // supply and not recipe
+            return can_cook[item]; // return if supply exists or not
+        }
+        if(can_cook.find(item) != can_cook.end()) { // recipe status already known
+            return can_cook[item];
+        }
+        // Do dfs and determine if its nei ingredients recipe/supply are cookable
+        // check for directed cycle 
+        can_cook[item] = false; // to detect/make cycle cases false, else inf loop
+        for(auto nei: ingredients[recipe_idx[item]]) {
+            if(!dfs(recipes,ingredients,recipe_idx,nei,can_cook)) {
+                return can_cook[item] = false;
+            }
+        }
+        return can_cook[item] = true;
+    }
+
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        int n = recipes.size();
+
+        unordered_map<string,bool> can_cook; // cookable supply and recipes
+        can_cook_supplies(can_cook,supplies); // mark base supply items
+
+        unordered_map<string,int> recipe_idx; // recipe: recipe_index
+        enumerateRecipe(recipes,recipe_idx);
+
+        vector<string> can_cook_recipes_list; // final ans
+
+        for(int i = 0; i < n; i++) {
+            if(dfs(recipes,ingredients,recipe_idx,recipes[i],can_cook)) {
+                can_cook_recipes_list.push_back(recipes[i]);
+            }
+        }
+
+        return can_cook_recipes_list;
+    }
+};
+
+// Approach 1: Topo Sort
+// 00:55:13
+class Solution1 {
 public:
     // convert items in vector to hashset and returns
     unordered_set<string> getItemSet(vector<string> &recipes) {
