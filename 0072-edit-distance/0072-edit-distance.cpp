f662397b9f3eec0c96c3e01426f56013437344e8
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-23 21:30:29 +0530

    Time: 14 ms (21.27%), Space: 13.3 MB (25.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,4 +1,29 @@
+// Approach 2: Dp Iterative
 class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        for(int j = 0; j <= n2; j++) // init
+            dp[0][j] = j;
+        for(int i = 0; i <= n1; i++) // init
+            dp[i][0] = i;
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
+
+// Approach 1: DP recursive
+class Solution1 {
 public:
     int dpf(string &word1, string &word2, int i, int j, vector<vector<int>> &dp) {
         if(i == 0 && j == 0) return 0;
@@ -16,7 +41,7 @@ public:
         int n1 = word1.size(), n2 = word2.size();
         vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
         int x = dpf(word1,word2,n1,n2,dp);
-        cout<<x<<endl;
+        // cout<<x<<endl;
         return x;
     }
 };
