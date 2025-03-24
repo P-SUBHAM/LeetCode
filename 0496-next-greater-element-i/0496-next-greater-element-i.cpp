74ab07969f948f31b64e38044b91743e8e215c32
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-24 20:04:49 +0530

    Time: 3 ms (45.69%), Space: 12.8 MB (51.73%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,15 +1,40 @@
-// Approach 1: Using monotonic stack
+class Solutionp {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st; // decrease mono stack
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums2.size(); i++) {
+            if(st.empty()) {
+                st.push(i); continue;
+            }
+            while(!st.empty() && nums2[i] > nums2[st.top()]) {
+                int cur = st.top(); st.pop();
+                mp[nums2[cur]] = nums2[i];
+            }
+            st.push(i);
+        }
+        vector<int> ans(nums1.size(),-1);
+        for(int i = 0; i < nums1.size(); i++) {
+            if(mp.find(nums1[i]) != mp.end()) {
+                ans[i] = mp[nums1[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+// Approach 1: Using monotonic decreasing stack
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> mp;
-        stack<int> mono_inc_st;
+        stack<int> mono_dec_st;
         for(int i = 0; i < nums2.size(); i++) {
-            while(!mono_inc_st.empty() && nums2[i] > nums2[mono_inc_st.top()]) {
-                int cur = mono_inc_st.top(); mono_inc_st.pop();
+            while(!mono_dec_st.empty() && nums2[i] > nums2[mono_dec_st.top()]) {
+                int cur = mono_dec_st.top(); mono_dec_st.pop();
                 mp[nums2[cur]] = nums2[i];
             }
-            mono_inc_st.push(i);
+            mono_dec_st.push(i);
         }
         vector<int> ans(nums1.size(),-1);
         for(int i = 0; i < nums1.size(); i++) {
