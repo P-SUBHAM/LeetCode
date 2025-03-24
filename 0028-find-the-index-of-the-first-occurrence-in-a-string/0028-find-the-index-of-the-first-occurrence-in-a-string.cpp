910b284157fe4b6023ab547c5a58d57051229d53
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-25 02:15:15 +0530

    Time: 15 ms (2.23%), Space: 8.6 MB (57.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,38 +1,94 @@
+// Approach 3.1 Rabin Karp
 class Solution {
 public:
-    vector<int> prefix_func(string needle) {
+    int mod = 1e9+7;
+    int p = 31;
+    int calcHash(string needle) {
         int n = needle.size();
-        vector<int> prefix(n);
-        int j = 0;
-        for(int i = 1; i < n; i++) {
-            j = prefix[i-1];
-            while(j > 0 && needle[i]!=needle[j]) {
-                j = prefix[j-1];
-            }
-            if(needle[i] == needle[j]) {
-                prefix[i] = j + 1;
-            }
+        int powp = 1;
+        int hash = 0;
+        for(int i = 0; i < n; i++) {
+            hash += (1ll*(needle[i]-'a'+1)*powp)%mod;
+            hash %= mod;
+            powp = (powp * (1ll*p))%mod;
         }
-        return prefix;
+        return hash;
+    }
+    int powr(int b, int power) {
+        if (power == 0) return 1;
+        int x = powr(b, power / 2);
+        x = (1LL * x * x) % mod;  // Squaring the result
+        if (power % 2 == 1) {
+            x = (1LL * x * b) % mod;  // If odd, multiply by base b
+        }
+        return x;
     }
     int strStr(string haystack, string needle) {
-        vector<int> prefix = prefix_func(needle);
-        for(auto it: prefix) cout<<it<<" "; cout<<endl;
-        int j = 0;
-        for(int i = 0; i < haystack.size(); i++) {// use while loop
-            cout<<i<<j<<endl;
-            if(haystack[i] == needle[j]) {
-                j++;
+        int needleHash = calcHash(needle);
+        int powp = 1;
+        int pinv = powr(p,mod-2);
+        int m = needle.size();
+        int n = haystack.size();
+        powp = powr(p,m-1);
+        cout<<powp<<":"<<pinv<<endl;
+        int rollingHash = calcHash(haystack.substr(0,m));
+        for(int i = 0; i < n - (m-1); i++) {
+            if(needleHash == rollingHash) return i;
+            cout<<needleHash<<":"<<rollingHash<<endl;
+            rollingHash = (rollingHash - (haystack[i]-'a'+1) + mod)%mod;
+            rollingHash = (1LL*rollingHash*pinv)%mod;
+            rollingHash = (1LL*rollingHash + 1LL*(haystack[i+m]-'a'+1)*powp)%mod;
+            // if (i < n - m) {
+            //     // Remove the old character from the hash and add the new character
+            //     rollingHash = (rollingHash - (haystack[i] - 'a' + 1) + mod) % mod;
+            //     rollingHash = (1LL * rollingHash * pinv) % mod;  // Use the inverse of p
+            //     rollingHash = (1LL * rollingHash + (haystack[i + m] - 'a' + 1) * powp) % mod;
+            // }
+        }
+        return -1;
+    }
+};
+
+
+
+// Approach 3: Rabin Karp string hashing
+class Solution3 {
+public:
+    int m = 1e9+7;
+    int p = 31;
+    int calcHash(string needle) {
+        int n = needle.size();
+        int powp = 1;
+        int hash = 0;
+        for(int i = 0; i < n; i++) {
+            hash += (1ll*(needle[i]-'a'+1)*powp)%m;
+            hash %= m;
+            powp = (powp * (1ll*p))%m;
+        }
+        return hash;
+    }
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        vector<long long> parr(n+1,1);
+        for(int i = 1; i <= n; i++) {
+            parr[i] = (1LL*parr[i-1] * p)%m;
+        }
+        // for(auto it: parr) cout<<it<<" "; cout<<endl; 
+        vector<long long> hayarr(n+1);
+        for(int i = 0; i < n; i++) {
+            hayarr[i+1] = (hayarr[i] + ((haystack[i] - 'a' + 1)*parr[i])%m)%m;
+        }
+        // for(auto it: hayarr) cout<<it<<" "; cout<<endl; 
+        int needlehash = calcHash(needle);
+        int sn = needle.size();
+        // p^i*(hash i..j) (hash j - hash i-1)
+        for(int i = 0; i < n - (sn-1); i++) {
+            int j = i + sn - 1;
+            int rollhash = (hayarr[j+1] - hayarr[i]);
+            cout<<rollhash<<":"<<needlehash * parr[i]<<endl;
+            if(needlehash * parr[i] == rollhash) {
+                return i;
             }
-            else {
-                bool con = false;
-                while(j > 0 && haystack[i] != needle[j]) {
-                    j = prefix[j-1]; con = true;
-                }
-                if(con) i--;
-                cout<<j<<endl;
-            }
-            if(j == needle.size()) return i + 1 - needle.size();
         }
         return -1;
     }
