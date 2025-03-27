4bd972e0d7b354fc9902df17ed804c7b657f3f46
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-03-27 17:03:18 +0530

    Time: 1040 ms (15.17%), Space: 289.6 MB (22.59%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,69 @@
+// Approach 1: Using Binary Trie to find Max Xor
+class Solution {
+public:
+    struct BinaryBitTrie {
+        vector<bool> getBinaryBits(int x) {
+            vector<bool> s;
+            for(int i = 31; i >= 0; i--) {
+                int ithBit = ((x >> i)&1);
+                s.push_back(ithBit);
+            }
+            return s;
+        }
+        struct node {
+            bool end = false;
+            node* next[2] = {}; // 0 1 // IMP define like this
+        };
+        node *root = new node;
+        int sz = 0;
+        void insert(int x) {
+            vector<bool> s = getBinaryBits(x);
+            auto it = root;
+            for(int i = 0; i < 32; i++) {
+                bool bit = s[i];
+                if(it->next[bit] == NULL) {
+                    it->next[bit] = new node;
+                }
+                it = it->next[bit];
+            }
+            it->end = true;
+            sz++;
+            // cout<<"ins:"<<x<<endl;
+        }
+        int xorMax(int x) {
+            if(sz == 0) return -1;
+            vector<bool> s = getBinaryBits(x);
+            auto it = root;
+            int ans = x;
+            for(int i = 0; i < 32; i++) {
+                bool bit = s[i];
+                bool altBit = (bit^1);
+                if(it->next[altBit] != NULL) {
+                    it = it->next[altBit];
+                    ans ^= (altBit<<(31-i));
+                }
+                else if(it->next[bit] != NULL) {
+                    it = it->next[bit];
+                    ans ^= (bit<<(31-i));
+                }
+                else {
+                    return -1;
+                }
+            }
+            return ans;
+        }
+    };
+    int findMaximumXOR(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        BinaryBitTrie trie;
+        trie.insert(nums[0]);
+        for(int i = 1; i < n; i++) {
+            int x = nums[i];
+            int maxXorV = trie.xorMax(x);
+            ans = max(ans,maxXorV);
+            trie.insert(x);
+        }
+        return ans;
+    }
+};
