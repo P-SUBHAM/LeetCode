144dce007140dedc12bbd036a6a6bb7b343c5e67
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-01 12:01:01 +0530

    Time: 2 ms (82.22%), Space: 119.6 MB (86.16%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,5 +1,23 @@
-// Approach 1: DP Memoization
+// Approach 2: DP Iterative
 class Solution {
+public:
+    // dp[i] = max(a[i][0]+dp[i+1+a[i][1]],dp[i+1]);
+    long long mostPoints(vector<vector<int>>& q) {
+        int n = q.size();
+        vector<long long> dp(n,-1);
+        for(int i = n-1; i >= 0; i--) {
+            long long take = q[i][0];
+            if(i+1+q[i][1] < n) take += dp[i+1+q[i][1]];
+            long long notTake = 0;
+            if(i+1 < n) notTake += dp[i+1];
+            dp[i] = max(take,notTake);
+        }
+        return dp[0];
+    }
+};
+
+// Approach 1: DP Memoization
+class Solution1 {
 public:
     long long dpf(vector<vector<int>>& questions, int i, int n, vector<long long> &dp) {
         if(i >= n) return 0;
