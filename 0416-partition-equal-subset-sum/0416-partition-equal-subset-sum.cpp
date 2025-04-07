ce84bd316b783a2000241f3191fdb77cd7383a11
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-07 14:18:21 +0530

    Time: 98 ms (71.41%), Space: 101.8 MB (16.47%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -13,12 +13,33 @@ public:
         bool notTake = dpf(nums,i-1,sum,dp);
         return dp[i][sum] = take || notTake;
     }
+    bool dpfIter2arr(vector<int> nums, int sum) {
+        int n = nums.size();
+        vector<int> prev(sum+1), curr(sum+1);
+        for(int j = 0; j <= sum; j++) {
+            if(nums[0] == j) {
+                prev[j] = true;
+            }
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < sum+1; j++) {
+                curr[j] = prev[j];
+                if(nums[i] <= j) {
+                    curr[j] = curr[j] || prev[j-nums[i]];
+                }
+            }
+            prev = curr;
+        }
+        return prev[sum];
+    }
+    // dp[i][sum] = dp[i-1][sum-a[i]] || dp[i-1][sum];
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum%2 == 1) return false;
         int subSum = sum/2;
         vector<vector<int>> dp(n, vector<int>(subSum+1,-1));
-        return dpf(nums,n-1,subSum,dp);
+        // return dpf(nums,n-1,subSum,dp);
+        return dpfIter2arr(nums,subSum);
     }
 };
