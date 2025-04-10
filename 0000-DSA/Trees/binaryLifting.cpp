7cbbe0ebed7674fb73b1638b4bb4631807992b28
P-SUBHAM <p.subham.software@gmail.com>
2025-04-10 11:44:43 +0530

    binary lifting

---
+++ 0000-DSA/Trees/binaryLifting.cpp
@@ -0,0 +1,24 @@
+struct BinaryLifting {
+    vector<vector<int>> up;
+    int lev;
+    void dfsBinaryLifting(int node, int par, vector<vector<int>> &tree) { // tree is directed graph
+        up[node][0] = par;
+        for (int i = 1; i <= lev && up[node][i-1]!=-1; i++)
+            up[node][i] = up[up[node][i-1]][i-1]; // 2^l = 2^l-1 * 2^l-1
+        for (auto child : tree[node])
+            dfsBinaryLifting(child, node, tree);
+    }
+    void preProcess(vector<vector<int>> tree, int n) {
+        lev = log2(n)+1;
+        up.resize(n+1,vector<int>(lev+1,-1));
+        dfsBinaryLifting(1,-1,tree);
+    }
+    int findKParent(int node, int k) {
+        if(k == 0 || node == -1) return node;
+        for(int i = lev; i >= 0; i--) {
+            if((k>>i)&1) {
+                return findKParent(up[node][i],k-(1<<i));
+            }
+        }
+    }
+};
