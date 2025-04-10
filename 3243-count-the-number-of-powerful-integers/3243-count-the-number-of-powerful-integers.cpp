23905588ea048975b8f4e0812d86f8d4750a9ea3
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-10 15:39:40 +0530

    Time: 7 ms (23.98%), Space: 10.6 MB (47.37%) - LeetHub

---
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
@@ -0,0 +1,31 @@
+// Approach 1: Digit dp
+class Solution {
+public:
+    long long dp[16][2]; // 10^15 has 16 digits
+    long long digitDPf(string &nums, int idx, bool tight, string &s, int &limit) {
+        // cout<<"i:"<<idx<<" "<<nums<<"\n";
+        if(idx > nums.size()) return 0;
+        if(nums.size()-idx == s.size()) { // end base case
+            if(!tight) return 1;
+            return nums.substr(idx,s.size()) >= s;
+        }
+        if(dp[idx][tight] != -1) return dp[idx][tight];
+        int lim = (tight)?min(limit,nums[idx]-'0'):limit;
+        long long ans = 0;
+        for(int i = 0; i <= lim; i++) {
+            ans += digitDPf(nums,idx+1,tight&(i==nums[idx]-'0'),s,limit);
+        }
+        return dp[idx][tight] = ans;
+    }
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        string strFin = to_string(finish);
+        string strSt = to_string(start-1);
+        
+        memset(dp,-1,sizeof(dp));
+        long long ub = digitDPf(strFin,0,1,s,limit);
+        memset(dp,-1,sizeof(dp)); // refresh dp table before each call
+        long long lb = digitDPf(strSt,0,1,s,limit);
+        // cout<<ub<<" "<<lb<<endl;
+        return ub - lb;
+    }
+};
