ecdedc404658e0c2f937d85d55d6baa9897e130b
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-13 02:02:57 +0530

    Time: 1704 ms (7.14%), Space: 460 MB (7.14%) - LeetHub

--- 3829-shortest-path-in-a-weighted-tree/3829-shortest-path-in-a-weighted-tree.cpp
+++ 3829-shortest-path-in-a-weighted-tree/3829-shortest-path-in-a-weighted-tree.cpp
@@ -1,76 +1,74 @@
 class Solution {
 public:
-    static const int NMAX = 100005;
-    int tin[NMAX], tout[NMAX], timer = 1;
-    long long initialDist[NMAX];
-    int parent[NMAX]; 
-    int parEdgeWeight[NMAX]; 
+    int timer = 1;
+    vector<int> tin;
+    vector<int> tout;
     vector<vector<vector<int>>> adj;
-    
-    struct BIT {
-        int n;
-        vector<long long> fenw;
-        BIT(int n): n(n) { fenw.assign(n + 1, 0); }
-        void update(int i, long long delta) {
-            for(; i <= n; i += i & -i)
-                fenw[i] += delta;
+    vector<int> edgeWt;
+    vector<int> parent;
+    void dfs(int node, int par) {
+        tin[node] = timer++;
+        parent[node] = par;
+        for(auto nei: adj[node]) {
+            int v = nei[0], w = nei[1];
+            if(v == par) continue;
+            edgeWt[v] = w;
+            dfs(v,node);
         }
+        tout[node] = timer++;
+    }
+    struct BIT {
+        vector<long long> fenw; int n;
+        BIT(int n_):n(n_) {fenw.resize(n_+2,0);}//0,1-n,n+1
         long long query(int i) {
             long long sum = 0;
-            for(; i > 0; i -= i & -i)
+            for(;i>0; i-=i&-i) 
                 sum += fenw[i];
             return sum;
         }
-        void range_update(int l, int r, long long delta) {
-            update(l, delta);
-            update(r + 1, -delta);
+        void update(int i, int diff) {
+            for(; i <= n; i += i&-i)
+                fenw[i] += diff;
         }
     };
-    
-    void dfs(int node, int par, long long dist) {
-        parent[node] = par;
-        initialDist[node] = dist;
-        tin[node] = timer++;
-        for (auto &edge : adj[node]) {
-            int nxt = edge[0], w = edge[1];
-            if (nxt == par)
-                continue;
-            parEdgeWeight[nxt] = w;
-            dfs(nxt, node, dist + w);
-        }
-        tout[node] = timer - 1;
-    }
-    
     vector<int> treeQueries(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        adj.clear();
-        adj.resize(n + 1);
-        for (auto edge : edges) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
-        }
-        
-        dfs(1, 0, 0);
-        BIT bit(n + 2);
+        tin.resize(n+1);
+        tout.resize(n+1);
+        adj.resize(n+1);
+        edgeWt.resize(n+1);
+        parent.resize(n+1);
         vector<int> ans;
-        for (auto query : queries) {
-            if (query[0] == 2) {
-                int node = query[1];
-                long long init_d = initialDist[node];
-                long long additional = bit.query(tin[node]);
-                long long tot = init_d + additional;
-                ans.push_back(tot);
+        for(auto edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        dfs(1,0);
+        BIT ftree(2*n); // for tin,tout
+        for(int i = 1; i <= n; i++) {
+            ftree.update(tin[i],edgeWt[i]);
+            ftree.update(tout[i],-1*edgeWt[i]);
+        }
+        for(auto query: queries) {
+            if(query[0] == 2) {
+                int dest = query[1];
+                // cout<<dest<<" ";
+                // cout<<ftree.query(tin[dest])<<endl;
+                ans.push_back(ftree.query(tin[dest]));
             }
-            else { 
+            else {
                 int u = query[1], v = query[2], w = query[3];
                 int child = v;
-                if (parent[u] == v)
-                    child = u;
-                int diff = w - parEdgeWeight[child];
-                parEdgeWeight[child] = w;
-                bit.range_update(tin[child], tout[child], diff);
+                if(parent[u] == v) child = u;
+                int oldw = edgeWt[child]; // any method is fine, only update ftree or edge wt arr too
+                oldw = ftree.query(tin[child]) - ftree.query(tin[child]-1);
+                ftree.update(tin[v],w-oldw);
+                ftree.update(tout[v],-w-(-oldw));
+                edgeWt[child] = w;
             }
         }
         return ans;
     }
 };
+
+/*n="4" edges = "[[1,2,2],[2,3,3],[3,4,4]]" query="[[2,4],[1,2,3,10],[2,3],[1,2,3,1],[2,3]]" ans="[9,12,3]"*/
