cbed0d8f5d2a6a1bf0222fd9f87d89d7dd338022
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-13 10:51:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (69.99%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -2,17 +2,29 @@
 class Solution {
 public:
     long long mod = 1e9+7;
-    long long pow(long long b, long long p) {
+    long long powR(long long b, long long p) {
+        b %= mod;
         if(p == 0) return 1;
-        if(p == 1) return b%mod;
+        if(p == 1) return b;
         long long x = pow(b,p/2);
-        x = x * x; x%=mod;
-        if(p%2==1) x = (x * b)%mod;
-        return x%mod;
+        x = (x * x) % mod;
+        if(p%2==1) x = (x * b) % mod;
+        return x;
+    }
+    // b^p = b^p0*b^p1*b^p2...
+    long long powI(long long b, long long p) {
+        b %= mod;
+        long long ans = 1;
+        while(p > 0) {
+            if(p & 1) ans = (ans * b) % mod;
+            b = (b * b) % mod;
+            p >>= 1;
+        }
+        return ans;
     }
     int countGoodNumbers(long long n) {
         long long odd = n/2;
         long long even = (n+1)/2;
-        return (pow(4,odd) * pow(5,even)) % mod;
+        return (powI(4,odd) * powI(5,even)) % mod;
     }
 };
