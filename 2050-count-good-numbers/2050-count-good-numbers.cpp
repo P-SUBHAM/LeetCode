3efaa5662f5e796c39ab4ed45a262434b7c72d94
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-13 10:57:33 +0530

    Time: 0 ms (100%), Space: 8 MB (34.93%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,30 +1,37 @@
-// Approach 1: Binary Exponentiation recursion
+// Approach 1: Binary Exponentiation
 class Solution {
 public:
-    long long mod = 1e9+7;
-    long long powR(long long b, long long p) {
-        b %= mod;
-        if(p == 0) return 1;
-        if(p == 1) return b;
-        long long x = pow(b,p/2);
-        x = (x * x) % mod;
-        if(p%2==1) x = (x * b) % mod;
-        return x;
-    }
-    // b^p = b^p0*b^p1*b^p2...
-    long long powI(long long b, long long p) {
-        b %= mod;
-        long long ans = 1;
-        while(p > 0) {
-            if(p & 1) ans = (ans * b) % mod;
-            b = (b * b) % mod;
-            p >>= 1;
+    struct power {
+        long long mod = 1e9+7;
+        long long powR(long long b, long long p) {
+            b %= mod;
+            if(p == 0) return 1;
+            if(p == 1) return b;
+            long long x = pow(b,p/2);
+            x = (x * x) % mod;
+            if(p%2==1) x = (x * b) % mod;
+            return x;
         }
-        return ans;
-    }
+        // b^p = b^p0*b^p1*b^p2...
+        long long powI(long long b, long long p) {
+            b %= mod;
+            long long ans = 1;
+            while(p > 0) {
+                if(p & 1) ans = (ans * b) % mod;
+                b = (b * b) % mod;
+                p >>= 1;
+            }
+            return ans;
+        }
+        long long operator()(long long b, long long p) {
+            return powI(b,p);
+            // return powR(b,p);
+        }
+    };
     int countGoodNumbers(long long n) {
+        int mod = 1e9+7;
         long long odd = n/2;
         long long even = (n+1)/2;
-        return (powI(4,odd) * powI(5,even)) % mod;
+        return (power()(4,odd) * power()(5,even)) % mod;
     }
 };
