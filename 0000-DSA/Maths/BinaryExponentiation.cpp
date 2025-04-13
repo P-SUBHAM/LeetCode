ae68b3b879f34975c495a26ef6a4ccf8d067bfdc
P-SUBHAM <p.subham.software@gmail.com>
2025-04-13 11:00:54 +0530

    binary exponentiation

---
+++ 0000-DSA/Maths/BinaryExponentiation.cpp
@@ -0,0 +1,29 @@
+struct power {
+    long long mod = 1e9+7;
+    long long powR(long long b, long long p) {
+        b %= mod;
+        if(p == 0) return 1;
+        if(p == 1) return b;
+        long long x = pow(b,p/2);
+        x = (x * x) % mod;
+        if(p%2==1) x = (x * b) % mod;
+        return x;
+    }
+    // b^p = b^p0*b^p1*b^p2...
+    long long powI(long long b, long long p) {
+        b %= mod;
+        long long ans = 1;
+        while(p > 0) {
+            if(p & 1) ans = (ans * b) % mod;
+            b = (b * b) % mod;
+            p >>= 1;
+        }
+        return ans;
+    }
+    long long operator()(long long b, long long p) {
+        return powI(b,p);
+        // return powR(b,p);
+    }
+};
+
+// power()(a,b); power() creates temp object
