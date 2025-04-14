cc40fbd1d048df82d8c2df99317fb340b976b6f2
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-14 21:00:41 +0530

    Update BSTIterator.cpp

--- 0000-DSA/Trees/BSTIterator.cpp
+++ 0000-DSA/Trees/BSTIterator.cpp
@@ -22,3 +22,58 @@ public:
         return !st.empty();
     }
 };
+
+class Solution {
+public:
+    struct bstIteratorLeft {
+        stack<TreeNode*> st;
+        void pushAll(TreeNode* root) {
+            while(root != NULL) {
+                st.push(root); root=root->left;
+            }
+        }
+        bstIteratorLeft(TreeNode* root) {
+            pushAll(root);
+        }
+        // Type operator++(int);  // Post-increment, the `int` is just a dummy
+        int operator++() { // pre-increment
+            if(st.empty()) return -1;
+            TreeNode *temp = st.top(); st.pop();
+            pushAll(temp->right);
+            return temp->val;
+        }
+        int operator*() {
+            return st.top()->val;
+        }
+    };
+    struct bstIteratorRight {
+        stack<TreeNode*> st;
+        void pushAll(TreeNode* root) {
+            while(root != NULL) {
+                st.push(root); root=root->right;
+            }
+        }
+        bstIteratorRight(TreeNode* root) {
+            pushAll(root);
+        }
+        int operator--(int) {
+            if(st.empty()) return -1;
+            TreeNode *temp = st.top(); st.pop();
+            pushAll(temp->left);
+            return temp->val;
+        }
+        int operator*() {
+            return st.top()->val;
+        }
+    };
+    bool findTarget(TreeNode* root, int k) {
+        bstIteratorLeft left(root);
+        bstIteratorRight right(root);
+        while(*left < *right) {
+            if(*left + *right == k) return true;
+            else if(*left + *right < k)  ++left;
+            else  right--;
+        }
+        return false;
+    }
+};
