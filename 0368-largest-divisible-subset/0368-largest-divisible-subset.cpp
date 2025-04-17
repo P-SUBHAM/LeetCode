f11b2799b8ef36cb48abf0f8c121b8cf432267f9
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-17 20:45:01 +0530

    Time: 19 ms (16.23%), Space: 13.9 MB (12.23%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,6 +1,44 @@
+// Approach 2: DP
+// fix i, search for next element which is mul of i, 
+// call dpf on next factor, store these factors index in a map to backtrack
+class Solution {
+public:
+    unordered_map<int,int> div;
+    int dpf(vector<int>& nums, int i, vector<int> &dp) {
+        int n = nums.size(), ans = 1;
+        if(i == n-1) return dp[i] = 1;
+        if(dp[i] != -1) return dp[i];
+        for(int j = i+1; j < n; j++) {
+            if(nums[j]%nums[i]==0 && 1+dpf(nums,j,dp) > ans) {
+                div[nums[i]] = j;
+                ans = max(ans,1+dpf(nums,j,dp));
+            }
+        }
+        return dp[i] = ans;
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        div.clear();
+        vector<int> dp(n,-1);
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i < n; i++) {
+            if(div.find(nums[i])==div.end()) div[nums[i]] = i;
+            ans = max(ans,dpf(nums,i,dp));
+        }
+        int ind = max_element(dp.begin(),dp.end()) - dp.begin();
+        vector<int> ansv;
+        while(div[nums[ind]] != ind) {
+            ansv.push_back(nums[ind]); ind = div[nums[ind]];
+        } ansv.push_back(nums[ind]);
+        // for(auto it: dp) cout<<it<<" "; cout<<endl;
+        // for(auto it: nums) cout<<div[it]<<" "; cout<<endl;
+        return ansv;
+    }
+};
+
 // Approach:1 
 // n^2 for every nums[i] check which nums[j] will increase the len by max, and nums[j] is factor of nums[i] and all till nums[j] are factors mutually 
-class Solution {
+class Solution1 {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(),nums.end());
