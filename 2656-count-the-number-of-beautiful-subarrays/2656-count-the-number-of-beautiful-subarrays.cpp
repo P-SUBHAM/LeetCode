a3b6979f4d66981b3ff98d0eeb4ce2649d727055
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-22 10:29:44 +0530

    Time: 1120 ms (5.36%), Space: 228 MB (5.36%) - LeetHub

---
+++ 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // Approach 1: Bit manip
+    // check how many ranges are there such that all index bits is even or xor0
+    void addBits(int x, vector<int> &bits) {
+        for(int i = 0; i <= 30; i++) {
+            if((x&(1<<i)) > 0) bits[i] ^= 1;
+        }
+    }
+    long long beautifulSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        vector<int> bits(30,0);
+        map<vector<int>,int> bitmp; bitmp[bits]++;
+        for(int i = 0; i < nums.size(); i++) {
+            addBits(nums[i],bits);
+            ans += bitmp[bits];
+            bitmp[bits]++;
+        }
+        return ans;
+    }
+};
