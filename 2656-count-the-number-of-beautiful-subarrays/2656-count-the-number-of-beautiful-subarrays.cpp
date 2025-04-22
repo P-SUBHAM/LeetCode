d91b8c791f4e64d971e33a6d02debef07726a657
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-22 10:32:25 +0530

    Time: 93 ms (96.55%), Space: 124.4 MB (80.08%) - LeetHub

--- 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.cpp
+++ 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    // check how many have number xor as 0
+    long long beautifulSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        unordered_map<int,int> mp; mp[0]++;
+        int cumxor = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            cumxor ^= nums[i];
+            ans += mp[cumxor]++;
+            // mp[cumxor]++;
+        }
+        return ans;
+    }
+};
+
+class Solution1 {
+public:
     // Approach 1: Bit manip
     // check how many ranges are there such that all index bits is even or xor0
     void addBits(int x, vector<int> &bits) {
