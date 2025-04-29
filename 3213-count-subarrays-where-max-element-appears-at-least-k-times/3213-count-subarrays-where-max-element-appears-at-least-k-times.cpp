1dc8e323d62b95f8e3c3c484955bcb2b6751a721
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-29 14:37:26 +0530

    Time: 8 ms (36.17%), Space: 121.6 MB (95.79%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -5,16 +5,28 @@ public:
     long long countSubarrays(vector<int>& nums, int k) {
         int maxv = *max_element(nums.begin(),nums.end());
         long long ans = 0, l = 0, r = 0, n = nums.size(), cnt = 0;
-        while(r < n) {
-            if(cnt < k) {
-                if(nums[r++] == maxv) {
-                    cnt++;
-                }
-            }
-            while(cnt == k) { // 3 3 1 4
+        // while(r < n) {
+        //     if(cnt < k) {
+        //         if(nums[r++] == maxv) {
+        //             cnt++;
+        //         }
+        //     }
+        //     while(cnt == k) { // 3 3 1 4
+        //         ans += n-1-(r-1)+1;
+        //         if(nums[l] == maxv) cnt--;
+        //         l++;
+        //     }
+        // }
+        while(r < n || cnt == k) {
+            if (cnt == k) { // 3 3 1 4
                 ans += n-1-(r-1)+1;
                 if(nums[l] == maxv) cnt--;
                 l++;
+            }
+            else if(cnt < k) {
+                if(nums[r++] == maxv) {
+                    cnt++;
+                }
             }
         }
         return ans;
