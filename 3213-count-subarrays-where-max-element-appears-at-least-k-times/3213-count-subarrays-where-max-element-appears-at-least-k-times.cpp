1b9806e34b3d29068a07b78e7198ae3b209ed3bf
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-04-29 14:32:48 +0530

    Time: 0 ms (100%), Space: 121.6 MB (74.24%) - LeetHub

---
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -0,0 +1,22 @@
+// Approach 1: 2ptr
+// while u can expand expand, once valid shrink and calc no of subarrays
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxv = *max_element(nums.begin(),nums.end());
+        long long ans = 0, l = 0, r = 0, n = nums.size(), cnt = 0;
+        while(r < n) {
+            if(cnt < k) {
+                if(nums[r++] == maxv) {
+                    cnt++;
+                }
+            }
+            while(cnt == k) { // 3 3 1 4
+                ans += n-1-(r-1)+1;
+                if(nums[l] == maxv) cnt--;
+                l++;
+            }
+        }
+        return ans;
+    }
+};
