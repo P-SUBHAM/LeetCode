43e308935e4afc2633151e4a4da5a99eba6b3ba8
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-05-01 22:22:11 +0530

    Update DSU.cpp

--- 0000-DSA/Graph/DSU.cpp
+++ 0000-DSA/Graph/DSU.cpp
@@ -1,5 +1,27 @@
+// DSU with union by size, get set size, if set exists
+struct DSU { // union by size
+    unordered_map<ll, ll> p; // Parent map
+    unordered_map<ll, ll> s; // size map
+    ll find_set(ll x) {
+        if (p.find(x) == p.end()) {s[x]=1; return p[x] = x;} // Lazy initialization
+        return (p[x] == x) ? x : p[x] = find_set(p[x]); // Path compression
+    }
+    void join_set(ll x, ll y) {
+        x = find_set(x), y = find_set(y);
+        if(x == y) return;
+        if(s[x] < s[y]) swap(x,y);
+        p[y] = x; s[x] += s[y];
+    }
+    bool exist_set(ll x) {
+        return (p.find(x) != p.end());
+    }
+    ll find_size(ll x) {
+        return s[find_set(x)];
+    }
+};
+
 // Simplest DSU implementation O(nlogn)
-struct dsu {
+struct DSU {
     unordered_map<int,int> p;
     int find(int x) {
         if(p.find(x)==p.end()) return p[x] = x;
@@ -12,7 +34,7 @@ struct dsu {
 // annecdote: map: TLE unordered_map:906ms 
 
 // DSU with path Compression and Union by rank
-struct dsu {
+struct DSU {
     unordered_map<int, int> p; // Parent map
     unordered_map<int, int> rank; // Rank map (optional, for union by rank)
     int find(int x) {
