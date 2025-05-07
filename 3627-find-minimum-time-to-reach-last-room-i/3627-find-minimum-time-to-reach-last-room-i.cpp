4c427751e1bc89fc88fc59cabc61ef33c39ceef9
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-05-07 18:45:35 +0530

    Time: 132 ms (5.06%), Space: 50.2 MB (5.06%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,30 @@
+// Approach 1: Priority Queue based Traversal
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size(), n = moveTime[0].size();
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        vector<vector<int>> vis(m,vector<int>(n,false));
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> pq;
+        pq.push({0,0,0});
+        auto valid = [&](int i, int j) {
+            return 0 <= i && i < m && 0 <= j && j < n;
+        };
+        while(!pq.empty()) {
+            auto node = pq.top(); pq.pop();
+            auto wt = node[0], i = node[1], j = node[2];
+            if(vis[i][j]) continue;
+            vis[i][j] = true;
+            // cout<<wt<<" "<<i<<" "<<j<<endl;
+            if(i == m-1 && j == n-1) return wt;
+            for(auto dir: dirs) {
+                int x = i+dir[0], y = j+dir[1];
+                if(valid(x,y)) {
+                    int new_wt = 1+max(wt,moveTime[x][y]);
+                    pq.push({new_wt,x,y});
+                }
+            }
+        }
+        return 0;
+    }
+};
