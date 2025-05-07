671d10ea995fb0992c507118206b3654aeb2c360
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-05-07 18:48:22 +0530

    Time: 125 ms (5.06%), Space: 50.2 MB (5.06%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,10 +1,10 @@
-// Approach 1: Priority Queue based Traversal
+// Approach 1: Priority Queue based Traversal with vis array
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int m = moveTime.size(), n = moveTime[0].size();
         vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-        vector<vector<int>> vis(m,vector<int>(n,false));
+        vector<vector<int>> vis(m,vector<int>(n,false)); // vis necessary 132ms else TLE
         priority_queue<vector<int>,vector<vector<int>>,greater<>> pq;
         pq.push({0,0,0});
         auto valid = [&](int i, int j) {
@@ -13,7 +13,7 @@ public:
         while(!pq.empty()) {
             auto node = pq.top(); pq.pop();
             auto wt = node[0], i = node[1], j = node[2];
-            if(vis[i][j]) continue;
+            if(vis[i][j]) continue; 
             vis[i][j] = true;
             // cout<<wt<<" "<<i<<" "<<j<<endl;
             if(i == m-1 && j == n-1) return wt;
