8beaa92e39e71def84a3c4162d708ed923f4d462
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-05-14 11:53:34 +0530

    Time: 80 ms (65.79%), Space: 26.1 MB (63.95%) - LeetHub

---
+++ 0972-knight-dialer/0972-knight-dialer.cpp
@@ -0,0 +1,86 @@
+// Dynamic Programming iterative
+class Solution {
+public:
+    static const int mod = 1e9+7;
+    bool isValid(int i, int j) {
+        return 0 <= i && i < 4 && 0 <= j && j < 3 && !(i==3&&j==0) && !(i==3&&j==2);
+    }
+    vector<vector<int>> moves = {{+2,+1},{+2,-1},{-2,+1},{-2,-1},{+1,+2},{+1,-2},{-1,+2},{-1,-2}};
+    static int dp[4][3][5001];
+    static bool precompf;
+    void precompute() {
+        precompf = false;
+        for(int i = 0; i < 12; i++) {
+            int r = i/3;
+            int c = i%3;
+            if(isValid(r,c)) {
+                dp[r][c][1] = 1;
+            }
+        }
+        for(int t = 2; t <= 5000; t++) {
+            for(int i = 0; i < 12; i++) {
+                int r = i/3;
+                int c = i%3;
+                if(!isValid(r,c)) continue;
+                for(auto move: moves) {
+                    if(isValid(r+move[0],c+move[1])) {
+                        dp[r][c][t] = (1ll*dp[r][c][t] + dp[r+move[0]][c+move[1]][t-1])%mod;
+                    }
+                }
+            }
+        }
+    }
+    int knightDialer(int n) {
+        if(precompf) {
+            precompute(); 
+        }
+        int ans = 0;
+        for(int i = 0; i < 12; i++) {
+            int r = i/3;
+            int c = i%3;
+            if(isValid(r,c)) {
+                ans = (1ll*ans + dp[r][c][n])%mod;
+            }
+        }
+        return ans;
+    }
+};
+int Solution::dp[4][3][5001];
+bool Solution::precompf = true;
+
+// Approach 1: Dynamic Programming
+class Solution1 {
+public:
+    static const int mod = 1e9+7;
+    bool isValid(int i, int j) {
+        return 0 <= i && i < 4 && 0 <= j && j < 3 && !(i==3&&j==0) && !(i==3&&j==2);
+    }
+    vector<vector<int>> moves = {{+2,+1},{+2,-1},{-2,+1},{-2,-1},{+1,+2},{+1,-2},{-1,+2},{-1,-2}};
+    int dp[4][3][5001];
+    int dpf(int r, int c, int n) {
+        if(!isValid(r,c)) return 0;
+        if(n == 1) return 1;
+        if(dp[r][c][n] != -1) {
+            // cout<<r<<" "<<c<<" "<<n<<":"<<dp[r][c][n]<<endl;
+            return dp[r][c][n];
+        }
+        int ans = 0;
+        for(auto move: moves) {
+            ans = (1ll*ans + dpf(r+move[0],c+move[1],n-1))%mod;
+        }
+        // cout<<r<<" "<<c<<" "<<n<<":"<<ans<<endl;
+        return dp[r][c][n] = ans;
+    }
+    int knightDialer(int n) {
+        int ans = 0;
+        memset(dp,-1,sizeof(dp)); // memset works for 0 or -1 only as byte based
+        for(int i = 0; i < 12; i++) {
+            int r = i/3;
+            int c = i%3;
+            if(isValid(r,c)) {
+                ans = (1ll*ans + dpf(r,c,n))%mod;
+            }
+        }
+        return ans;
+    }
+};
