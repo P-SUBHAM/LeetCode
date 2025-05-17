61c97a76c1839059064904e7d980de421ecc4b16
P-SUBHAM <34962038+P-SUBHAM@users.noreply.github.com>
2025-05-17 11:01:48 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,5 +1,22 @@
-// Approach 1: DNF sort
 class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0, k = nums.size()-1;
+        while(j <= k) {
+            // for(auto it: nums) cout<<it<<" "; cout<<endl;
+            // for(int x = 0; x < nums.size(); x++) {
+            //     if(x == i || x == j || x == k) cout<<"^ ";
+            //     else cout<<"  ";
+            // }cout<<endl;
+            if(nums[j] == 0) swap(nums[i++],nums[j++]);// in start j, i if no j++;
+            else if(nums[j] == 2) swap(nums[j],nums[k--]);
+            else if(nums[j++] == 1) {}
+        }
+    }
+};
+
+// Approach 1: DNF sort
+class Solution1 {
 public:
     void sortColors(vector<int>& nums) {
         int l = 0, m = 0, n = nums.size(), h = n - 1;
